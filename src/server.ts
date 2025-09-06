080fb68593f4479279d669edea6eeab2f8c5b3a8
codewithMERN <88480039+Crazy-Zaini007@users.noreply.github.com>
2025-09-07 00:54:36 +0500

    methods in cors updated

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ app.use(cors({
     'https://gitexai-1dd08c5fca2d.herokuapp.com',
     'http://localhost:3000'             
   ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
