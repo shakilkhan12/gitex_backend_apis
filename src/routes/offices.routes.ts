d8d8183e258b7c4fecb5dbbf1a5775a659c3b998
shakilkhan12 <shakilkhan621@gmail.com>
2025-09-06 12:04:05 +0500

    created new apis

--- src/routes/offices.routes.ts
+++ src/routes/offices.routes.ts
@@ -1,12 +1,14 @@
 import { OfficesController } from "@/controllers";
-import { officeBasicInfoValidations, officeValidations } from "@/validations";
+import { officeBasicInfoValidations, officeValidations, validateOfficeCamera } from "@/validations";
 import { Router } from "express";
 const officesRouter  = Router();
 officesRouter.post('/add',officeValidations, OfficesController.addOffice)
-officesRouter.post('/add-office-camera', officeValidations, OfficesController.addOfficeCamera)
+officesRouter.post('/add-office-camera', validateOfficeCamera, OfficesController.addOfficeCamera)
 officesRouter.put("/update-office-camera-function",OfficesController.changeOfficeCameraFunctionality)
 officesRouter.put('/update-office-setting', OfficesController.updateOfficeSetting)
 officesRouter.put('/update-office-basic-info', officeBasicInfoValidations, OfficesController.updateOfficeBasicInfo);
+officesRouter.put('/update-office-image', OfficesController.updateOfficeImage)
+officesRouter.put('/update-office-camera/:id', validateOfficeCamera, OfficesController.updateOfficeCamera)
 officesRouter.get('/get', OfficesController.getOffices)
 officesRouter.get('/get-office/:office_Id', OfficesController.getOffice)
 officesRouter.get('/get-office-cameras/:officeId', OfficesController.getOfficeCameras)
