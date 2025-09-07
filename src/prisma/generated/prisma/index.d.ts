b89d05f3333afeddecb0908d7f34faf2d7e825a2
shakilkhan12 <shakilkhan621@gmail.com>
2025-09-07 12:50:18 +0500

    ingnored package lock file

--- src/prisma/generated/prisma/index.d.ts
+++ src/prisma/generated/prisma/index.d.ts
@@ -632,8 +632,8 @@ export namespace Prisma {
   export import Exact = $Public.Exact
 
   /**
-   * Prisma Client JS version: 6.14.0
-   * Query Engine version: 717184b7b35ea05dfa71a3236b7af656013e1e49
+   * Prisma Client JS version: 6.15.0
+   * Query Engine version: 85179d7826409ee107a6ba334b5e305ae3fba9fb
    */
   export type PrismaVersion = {
     client: string
