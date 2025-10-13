d2ed2b99af9317f076196f477617fca16e75ca33
Fazli Mola Jan <fazal@codeforpakistan.org>
2025-10-14 00:18:09 +0400

    sentiment updated

--- src/services/park-sentiment-analysis.service.ts
+++ src/services/park-sentiment-analysis.service.ts
@@ -178,7 +178,7 @@ class ParkSentimentAnalysisService {
                }
             },
             orderBy: {
-               createdAt: 'desc'
+               updatedAt: 'desc'
             }
          });
 
